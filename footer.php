585c290807984cb734304eb564485ca5bf45952c
Tyrone Roberts <hi@tyronejroberts.co.uk>
2022-11-13 20:15:30 +0000

    Handle app links

--- footer.php
+++ footer.php
@@ -1,8 +1,25 @@
         </main>
           <script>
-            window.addEventListener('load', function () {
-                window.ReactNativeWebView.postMessage(JSON.stringify({ type: 'Loaded', height: document.documentElement.scrollHeight }));
-            });
+
+            (function() {
+                if (!window.ReactNativeWebView) return;
+
+                var aTags = document.getElementsByTagName('a');
+
+                for (var i = 0; i < aTags.length; i++) {
+                  aTags[i].addEventListener('click', function(e) {
+                    e.preventDefault();
+                    var href = e.currentTarget.href;
+                    window.ReactNativeWebView.postMessage(JSON.stringify({ type: 'LinkClick', href: href }));
+                  });
+                }
+
+                window.addEventListener('load', function () {
+                  window.ReactNativeWebView.postMessage(JSON.stringify({ type: 'Loaded', height: document.documentElement.scrollHeight }));
+                });
+            }())
+
+            
           </script>
 		<?php wp_footer(); ?>
     </body>
